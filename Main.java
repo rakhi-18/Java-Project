c7e54295667f2bf348cf95c19cfba75c7ff2a05e
Rakhi Katankar <rakhikatankar18@gmail.com>
2026-09-22 09:27:44 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+class Wallet {
+    private double balance;
+    static double cashbackRate = 0.02;
+    public Wallet() {
+        this.balance = 0;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("Invalid deposit amount!");
+        }
+    }
+public void transferTo(Wallet receiver, double amount) {
+    if (amount <= 0) {
+        System.out.println("Invalid amount entered!");
+        return;
+    }
+    if (this.balance < amount) {
+        System.out.println("Insufficient balance!");
+        return;
+    }
+    this.balance -= amount;
+    receiver.balance += amount;
+    double cashback = amount * cashbackRate;
+    this.balance += cashback;
+    System.out.println("Transaction successful!");
+    System.out.println("Cashback received: " + cashback);
+}
+}
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        Wallet w1 = new Wallet();
+        Wallet w2 = new Wallet();
+        int choice;
+        double amount;
+        do {
+            System.out.println("\nDIGITAL WALLET MENU:");
+            System.out.println("1. Deposit to Wallet 1");
+            System.out.println("2. Deposit to Wallet 2");
+            System.out.println("3. Transfer from Wallet 1 to Wallet 2");
+            System.out.println("4. Check Balances");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter the amount to deposit in Wallet 1: ");
+                    amount = sc.nextDouble();
+                    w1.deposit(amount);
+                    break;
+                case 2:
+                    System.out.print("Enter the amount to deposit in Wallet 2: ");
+                    amount = sc.nextDouble();
+                    w2.deposit(amount);
+                    break;
+                case 3:
+                    System.out.print("Enter the amount to transfer: ");
+                    amount = sc.nextDouble();
+                    w1.transferTo(w2, amount);
+                    break;
+                case 4:
+                    System.out.println("Wallet 1 Balance: " + w1.getBalance());
+                    System.out.println("Wallet 2 Balance: " + w2.getBalance());
+                    break;
+                case 5:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice, Please try again");
+            }
+        } while(choice != 5);
+    }
+}
